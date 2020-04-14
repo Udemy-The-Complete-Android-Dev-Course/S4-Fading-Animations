4eb4d145a8f97fc21ace019b981109fb2c1400a7
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-14 13:45:10 +0100

    Add scale and rotation transformations.

--- app/src/main/java/ldansorean/s4fadinganimations/MainActivity.java
+++ app/src/main/java/ldansorean/s4fadinganimations/MainActivity.java
@@ -10,7 +10,9 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private static int ANIMATION_DURATION = 2000;
-    private float roseAlpha = 1;
+    private static int ROTATIONS = 360 * 5;
+    private float roseAlpha = 1, roseScale = 1;
+    private float yellowAlpha = 0, yellowScale = 0;
     private ImageView roseImg, yellowImg;
 
     @Override
@@ -21,20 +23,30 @@ public class MainActivity extends AppCompatActivity {
         roseImg = findViewById(R.id.rose);
         yellowImg = findViewById(R.id.yellow);
 
-        //set initial transparency
+        //set initial transparency and size. Rose is full size and visible, yellow is transparent and small.
         roseImg.setAlpha(roseAlpha);
-        yellowImg.setAlpha(1-roseAlpha);
+        yellowImg.setAlpha(yellowAlpha);
+        yellowImg.setScaleX(yellowScale);
+        yellowImg.setScaleY(yellowScale);
     }
 
     /**
-     * Swap transparency between images using animation.
-     * When one image is visible the other is transparent.
+     * Swap image in focus using animations.
      */
     public void animate(View view) {
         Log.i("animate", getResources().getResourceEntryName(view.getId()) + " was clicked.");
 
+        //flip sizes and alpha
         roseAlpha = 1 - roseAlpha;
-        roseImg.animate().alpha(roseAlpha).setDuration(ANIMATION_DURATION);
-        yellowImg.animate().alpha(1 - roseAlpha).setDuration(ANIMATION_DURATION);
+        roseScale = 1 - roseScale;
+        yellowAlpha = 1 - yellowAlpha;
+        yellowScale = 1 - yellowScale;
+
+        Log.i("animate", String.format("Rose: alpha = %.2f, scale = %.2f", roseAlpha, roseScale));
+        Log.i("animate", String.format("Yellow: alpha = %.2f, scale = %.2f", yellowAlpha, yellowScale));
+
+        //animate new size and alpha
+        roseImg.animate().alpha(roseAlpha).scaleX(roseScale).scaleY(roseScale).rotationBy(ROTATIONS).setDuration(ANIMATION_DURATION);
+        yellowImg.animate().alpha(yellowAlpha).scaleX(yellowScale).scaleY(yellowScale).rotationBy(ROTATIONS).setDuration(ANIMATION_DURATION);
     }
 }
